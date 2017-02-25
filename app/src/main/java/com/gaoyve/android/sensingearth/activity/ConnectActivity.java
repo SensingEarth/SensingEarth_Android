50fe51b3820177666fec3d3d0ae8d6e1da3a9fbf
Gerry Gao <hologerry@gmail.com>
2017-02-25 20:36:52 +0800

    add port regex, fix crash when typed non-numeric string

--- app/src/main/java/com/gaoyve/android/sensingearth/activity/ConnectActivity.java
+++ app/src/main/java/com/gaoyve/android/sensingearth/activity/ConnectActivity.java
@@ -3,11 +3,16 @@ package com.gaoyve.android.sensingearth.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.gaoyve.android.sensingearth.R;
+
+import java.util.regex.Pattern;
 
 /**
  * SensingEarth
@@ -18,6 +23,7 @@ import com.gaoyve.android.sensingearth.R;
 public class ConnectActivity extends AppCompatActivity {
     private EditText mPortEditText;
     private Button mConnectButton;
+    private final String PORT_REGEX = "^([1-9]|[1-9]\\d{1,3}|[1-6][0-5][0-5][0-3][0-5])$"; // 1-65535
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +38,19 @@ public class ConnectActivity extends AppCompatActivity {
     private View.OnClickListener connectButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            int port = Integer.parseInt(mPortEditText.getText().toString().trim());
-            Intent intent = StartActivity.newIntent(ConnectActivity.this, port);
-            startActivity(intent);
-            System.out.println("ConnectButton Clicked");
+            String portStr = mPortEditText.getText().toString().trim();
+            boolean inputValid = Pattern.matches(PORT_REGEX, portStr);
+            if (inputValid) {
+                int port = Integer.parseInt(portStr);
+                Intent intent = StartActivity.newIntent(ConnectActivity.this, port);
+                startActivity(intent);
+                System.out.println("ConnectButton Clicked");
+            } else {
+                Toast toast = Toast.makeText(ConnectActivity.this, R.string.port_error,Toast.LENGTH_LONG);
+                TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+                if( v != null) v.setGravity(Gravity.CENTER);
+                toast.show();
+            }
         }
     };
 }
